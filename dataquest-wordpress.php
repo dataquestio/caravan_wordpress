ffaf428019d0708b820e801de8d4068d625bedaf
Vik Paruchuri <vik.paruchuri@gmail.com>
2017-09-19 13:34:50 -0700

    Switch naming to caravan

--- dataquest-wordpress.php
+++ dataquest-wordpress.php
@@ -1,21 +1,21 @@
 <?php
 /**
- * @package DataquestWordpress
+ * @package DataquestCaravan
  * @version 1.0
  */
 /*
-Plugin Name: Dataquest Wordpress
-Description: Embeds a light version of Dataquest Interactive Embedded Coding System into a post
+Plugin Name: Dataquest Caravan
+Description: Enables embedding the Dataquest code running interface and answer checking into a blog post.
 Version: 1.0
 Author URI: https://dataquest.io/
-Author: Dataquest.io
-GitHub: https://github.com/dataquestio/interactive_code_embed
+Author: Dataquest
+GitHub: https://github.com/dataquestio/caravan_wordpress
 */
 // Code written by Curtis M. Humphrey, Ph.D.
 
 if (!defined('ABSPATH')) exit;
 
-class DataquestWordpress {
+class DataquestCaravan {
   public static function load_scripts() {   
     wp_enqueue_script("dataquest-postbox", '//dqeditor.dataquest.io/dq_post_box.js');
     wp_enqueue_script("dataquest-helper", plugins_url('/dq_helper.js' , __FILE__ ), array('jquery'));
@@ -24,9 +24,9 @@ class DataquestWordpress {
   public static function show_media_button() {
     echo  '<a href="" class="button" ' .
             'id="insert-dataquest-exercise-button" ' .
-            'title="' . __("Insert Dataquest Exercise Template", 'add_dataquest_exercise') .
+            'title="' . __("Insert Dataquest Caravan Exercise Template", 'add_dataquest_exercise') .
           '">' .
-            'Add Dq Template' .
+            'Add Caravan Template' .
           '</a>';
   }
 
@@ -42,6 +42,6 @@ class DataquestWordpress {
 }
 
 // Run plugin
-DataquestWordpress::run();
+DataquestCaravan::run();
 
 ?>
