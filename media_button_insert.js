b20fa2e948323a66972f94a61920b407c3cc7cd1
Vik Paruchuri <vik.paruchuri@gmail.com>
2017-09-19 13:35:56 -0700

    Update media_button_insert.js

--- media_button_insert.js
+++ media_button_insert.js
@@ -1,4 +1,4 @@
-var output = "<iframe name=\"dq_editor\" width=\"100%\" height=700 >\n  <pre id=\"initialization-code\">\n  </pre>\n  <pre id=\"code\">\n  </pre>\n  <pre id=\"hint\">\n    # markdown here\n  </pre>\n  <pre id=\"answer-code\" check-vars=\"\" check-stdout >\n  </pre>\n</iframe>\n"; // eslint-disable-line
+var output = "<iframe name=\"dq_editor\" width=\"100%\" height=\"350\" >\n  <pre id=\"initialization-code\">\n  </pre>\n  <pre id=\"code\">\n  </pre>\n  <pre id=\"hint\">\n    # markdown here\n  </pre>\n  <pre id=\"answer-code\" check-vars=\"\" >\n  </pre>\n</iframe>\n"; // eslint-disable-line
 
 jQuery (function ($) { // eslint-disable-line
   $(document).ready(function () {
